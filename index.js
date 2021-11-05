8bf11f87f5202bca313307e8159d886662c88658
Bing Huang <bing2609@gmail.com>
2021-11-06 03:01:36 +0800

    initial implementation of mysql
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
+const { init: initMySql } = require("./lib/mysql-wrapper")
 const app = express()
 const port = 3000
+
+initMySql()
 
 app.use(express.static("public"))
 
