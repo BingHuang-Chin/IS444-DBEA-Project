b9c30bb18ece6304259c57eab7ccbf4f08a18901
Bing Huang <bing2609@gmail.com>
2021-11-06 03:23:01 +0800

    initial implementation of auth route and database script
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const { init: initMySql } = require("./lib/mysql-wrapper")
+
+const authRouter = require("./routes/auth")
+
 const app = express()
 const port = 3000
 
 initMySql()
 
 app.use(express.static("public"))
+
+app.use("/auth", authRouter)
 
 app.listen(port, () => console.log(`Application running at http://localhost:${port}`))
