0a344eb7bd3632aae4e854469c21d240000e33f5
Bing Huang <bing2609@gmail.com>
2021-11-09 15:31:44 +0800

    routes: auth: added logic to handle user creation in db
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,34 @@
 const express = require("express")
 const router = express.Router()
+const mySql = require("../lib/mysql-wrapper")
 
-router.post("/login", (req, res) => {
-  res.json({ hello: "world" })
+router.post("/login", async (req, res) => {
+  const { accessToken, isLender } = req.body
+  if (!accessToken)
+    return res.json({ status: 401, message: "Unauthorised access." })
+
+  const { userID: userId } = accessToken
+  const results = await mySql.handleQuery(`
+    SELECT * FROM fc_user
+    WHERE user_id = ?
+  `, [userId])
+
+  if (results.length === 0)
+    await createUser(userId, isLender)
+
+  res.json({ 
+    status: 200,
+    message: "Successfully registered with FastCash.",
+    redirect: `/pages/${isLender ? "lender.html" : "borrower.html"}`
+  })
 })
 
 module.exports = router
+
+async function createUser(userId, isLender) {
+  await mySql.handleQuery(`
+    INSERT INTO fc_user (user_id, is_lender)
+    VALUES
+    (?, ?)
+  `, [userId, JSON.parse(isLender)])
+}
