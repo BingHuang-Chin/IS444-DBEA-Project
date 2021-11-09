067e39e88377ade9f9b2f445e431e3ddb07a0a95
Bing Huang <bing2609@gmail.com>
2021-11-09 15:45:57 +0800

    index: added depositRouter to /desposit route
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const { init: initMySql } = require("./lib/mysql-wrapper")
 
 const authRouter = require("./routes/auth")
+const depositRouter = require("./routes/deposit")
 
 const app = express()
 const port = 3000
@@ -14,5 +15,6 @@ app.use(express.json())
 app.use(express.static("public"))
 
 app.use("/api/auth", authRouter)
+app.use("/api/deposit", depositRouter)
 
 app.listen(port, () => console.log(`Application running at http://localhost:${port}`))
