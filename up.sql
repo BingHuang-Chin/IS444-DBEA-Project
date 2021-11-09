cec27735fc0166d5931011f9b179acea27d6e5c1
Bing Huang <bing2609@gmail.com>
2021-11-09 15:56:48 +0800

    up.sql: added loan and loan_status tables
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- up.sql
+++ up.sql
@@ -6,3 +6,29 @@ CREATE TABLE fc_user (
 	is_lender		BOOL			NOT NULL	DEFAULT 	FALSE,
 	created_at		TIMESTAMP 		NOT NULL	DEFAULT		CURRENT_TIMESTAMP()
 );
+
+CREATE TABLE loan_status (
+	id				INT				AUTO_INCREMENT,
+	title			VARCHAR(50)		NOT NULL,
+	
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE loans (
+	id					INT				AUTO_INCREMENT,
+	user_id				VARCHAR(255)	NOT NULL,
+	loan_amount			INT				NOT NULL,
+	loan_status			INT				NOT NULL	DEFAULT	1,
+	loaned_by			VARCHAR(255),
+	payment_duration	INT,
+	due_date			TIMESTAMP,
+	interest			INT				DEFAULT	15,
+	
+	PRIMARY KEY (id)
+);
+
+INSERT INTO loan_status (title)
+VALUES
+('Open'),
+('Offered'),
+('Rejected');
