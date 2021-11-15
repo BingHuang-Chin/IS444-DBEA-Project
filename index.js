cdff9092f7912d7e043a1736e59989c79b71bedb
Bing Huang <bing2609@gmail.com>
2021-11-15 11:28:39 +0800

    index: added peer-to-peer lending routes
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { init: initMySql } = require("./lib/mysql-wrapper")
 const authRouter = require("./routes/auth")
 const depositRouter = require("./routes/deposit")
 const loanRouter = require("./routes/loan")
+const peerRouter = require("./routes/peer-lending")
 
 const app = express()
 const port = 3000
@@ -18,5 +19,6 @@ app.use(express.static("public"))
 app.use("/api/auth", authRouter)
 app.use("/api/deposit", depositRouter)
 app.use("/api/loan", loanRouter)
+app.use("/api/peer", peerRouter)
 
 app.listen(port, () => console.log(`Application running at http://localhost:${port}`))
