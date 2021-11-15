9d5b5bf943e6fba67fed11f52a3f356cd30d88b3
Bing Huang <bing2609@gmail.com>
2021-11-15 11:24:37 +0800

    routes: peer-lending: added logic for adding committed lending
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- routes/peer-lending.js
+++ routes/peer-lending.js
@@ -2,8 +2,45 @@ const express = require("express")
 const router = express.Router()
 const mySql = require("../lib/mysql-wrapper")
 
+/**
+ * Post a peer-to-peer lending listing
+ */
 router.post("/", async (req, res) => {
-  res.json({ initial: "implementation" })
+  const { accessToken, commitedAmount, interestRate } = req.body
+
+  if (!accessToken)
+    return res.json({ status: 401, message: "Unauthorized access." })
+
+  if (!commitedAmount || commitedAmount < 250)
+    return res.json({ status: 400, message: "Commit amount cannot be lower than $250." })
+
+  if (!interestRate || interestRate < 1 || interestRate > 55)
+    return res.json({ status: 400, message: "Interest rate cannot be lower than 1 and higher than 55." })
+
+  try {
+    await createListing({ userId: accessToken.userID, commitedAmount, interestRate })
+    return res.json({ status: 200, message: "Successfully listed." })
+  } catch (e) {
+    console.error(e)
+    return res.json({ status: 500, message: "Internal server error" })
+  }
 })
+
+async function getListings () {
+  return mySql.handleQuery(`
+    SELECT *
+    FROM peer_listing;
+  `)
+}
+
+async function createListing (record) {
+  const { userId, commitedAmount, interestRate } = record
+
+  return mySql.handleQuery(`
+    INSERT INTO peer_listing (commited_amount, interest_rate, listed_by)
+    VALUES
+    (?, ?, ?)
+  `, [commitedAmount, interestRate, userId])
+}
 
 module.exports = router
