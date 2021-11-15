494402ac89ce8096914813ea55e5cd50de916bb3
Bing Huang <bing2609@gmail.com>
2021-11-15 11:28:16 +0800

    routes: peer-lending: added route to retrieve all peer-to-peer listing
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- routes/peer-lending.js
+++ routes/peer-lending.js
@@ -3,22 +3,40 @@ const router = express.Router()
 const mySql = require("../lib/mysql-wrapper")
 
 /**
- * Post a peer-to-peer lending listing
+ * Retrieves all lenders amount
  */
-router.post("/", async (req, res) => {
-  const { accessToken, commitedAmount, interestRate } = req.body
+router.get("/", async (req, res) => {
+  const { accessToken } = req.body
 
   if (!accessToken)
     return res.json({ status: 401, message: "Unauthorized access." })
 
-  if (!commitedAmount || commitedAmount < 250)
+  try {
+    const peerList = await getListings()
+    return res.json({ status: 200, data: peerList })
+  } catch (e) {
+    console.error(e)
+    return res.json({ status: 500, message: "Internal server error" })
+  }
+})
+
+/**
+ * Post a peer-to-peer lending listing
+ */
+router.post("/", async (req, res) => {
+  const { accessToken, committedAmount, interestRate } = req.body
+
+  if (!accessToken)
+    return res.json({ status: 401, message: "Unauthorized access." })
+
+  if (!committedAmount || committedAmount < 250)
     return res.json({ status: 400, message: "Commit amount cannot be lower than $250." })
 
   if (!interestRate || interestRate < 1 || interestRate > 55)
     return res.json({ status: 400, message: "Interest rate cannot be lower than 1 and higher than 55." })
 
   try {
-    await createListing({ userId: accessToken.userID, commitedAmount, interestRate })
+    await createListing({ userId: accessToken.userID, committedAmount, interestRate })
     return res.json({ status: 200, message: "Successfully listed." })
   } catch (e) {
     console.error(e)
@@ -34,13 +52,13 @@ async function getListings () {
 }
 
 async function createListing (record) {
-  const { userId, commitedAmount, interestRate } = record
+  const { userId, committedAmount, interestRate } = record
 
   return mySql.handleQuery(`
     INSERT INTO peer_listing (commited_amount, interest_rate, listed_by)
     VALUES
     (?, ?, ?)
-  `, [commitedAmount, interestRate, userId])
+  `, [committedAmount, interestRate, userId])
 }
 
 module.exports = router
