4530f32fdb3b1b8a919210d3cd7e7ce79e80c590
Bing Huang <bing2609@gmail.com>
2021-11-15 11:07:08 +0800

    routes: peer-lending: initial implementation
    Signed-off-by: Bing Huang <bing2609@gmail.com>

---
+++ routes/peer-lending.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const router = express.Router()
+const mySql = require("../lib/mysql-wrapper")
+
+router.post("/", async (req, res) => {
+  res.json({ initial: "implementation" })
+})
+
+module.exports = router
