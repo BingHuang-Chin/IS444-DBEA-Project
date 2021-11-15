25be96689b57cd7657c0545622e32186db66fa9a
WYZ404 <yizhou.wang.2017@sis.smu.edu.sg>
2021-11-15 21:28:51 +0800

    add loan list page
    add loan list page

--- public/pages/lender_loanlist.php
+++ public/pages/lender_loanlist.php
@@ -7,9 +7,8 @@
   <title>FastCash | Lender's</title>
 
   <!-- Bootstrap core CSS -->
-  
-
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 
     <style>
       .bd-placeholder-img {
@@ -33,7 +32,7 @@
 
 <body>
   <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">FastCash</a>
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="">FastCash</a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -61,13 +60,13 @@
            </li>
           <br>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="lender.html">
+              <a class="nav-link" aria-current="page" href="lender.html">
                 <span data-feather="home"></span>
                 Dashboard
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="lender_loanpool.html">
+              <a class="nav-link active" href="lender_loanpool.html">
                 <span data-feather="users"></span>
                 Loan Pool
               </a>
@@ -81,7 +80,7 @@
               <a class="nav-link" href="loan_list.html">
                 <span data-feather="bar-chart-2"></span>
                 Loan List
-              </a>
+                </a>
           </ul>
   
           <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
@@ -99,32 +98,51 @@
           </ul>
         </div>
       </nav>
-
-      <!-- Need to discuss what content to display on the dashboard -->
   
-      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 class="h2">Dashboard</h1>
-          <div class="btn-toolbar mb-2 mb-md-0">
-            <div class="btn-group me-2">
-              <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-              <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
-            </div>
-            <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-              <span data-feather="calendar"></span>
-              This week
-            </button>
-          </div>
-        </div>
-  
-        <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
-
-      </main>
+    <?php
+      $servername = "localhost";
+      $username = "root";
+      $password = "root";
+      $conn = new mysqli($servername,$username,$password);
+      if($conn->connect_error){
+        die("connection failed: " . $conn->connect_error);
+      }
+      $sql = "SELECT id, user_id, loan_amount, loan_status, loaned_by,
+      payment_duration, due_date, interest
+      FROM loans
+      $result = $conn->query($sql);
+      
+      if ($result->num_rows > 0) {
+      
+          echo"<table>";
+          echo("<table border = \"1\">");
+          print("<tr>");
+          print("<th>ID</th>");
+          print("<th>USER ID</th>");
+          print("<th>LOAN AMOUNT</th>");
+          print("<th>LOAN STATUS</th>");
+          print("<th>LOANED BY</th>");
+          print("<th>PAYMENT DURATION</th>");
+          print("<th>DUE DATE</th>");
+          print("<th>INTEREST</th>");
+          while($row = $result->fetch_assoc()) {
+              echo "<tr><td>" . $row["loans.id"]. "</td><td>" . $row["loans.user_id"].
+              "</td><td>" . $row["loans.loan_amount"]. "</td><td>" . $row["loans.loan_status"] . "</td><td>" .
+              $row["loans.payment_duration"] . "</td><td>" . $row["loans.due_date"] . "</td><td>" .
+              $row["loans.interest"] . "</td><td>";
+          }
+      } else {
+          echo "0 results";
+      }
+      echo"</table>";
+      $conn->close();
+      
+      ?>  
     </div>
   </div>
   
   
-      <script src="/js/bootstrap.bundle.min.js"></script>
+      <script src="../../assets/dist/js/bootstrap.bundle.min.js"></script>
   
         <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="../../dashboard.js"></script>
 </body>
