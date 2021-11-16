c21f8916017e615fd6b7173cd78ca27ea9d40c50
Bing Huang <bing2609@gmail.com>
2021-11-17 00:17:01 +0800

    routes: loan: added logic to handle requesting of loan
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- routes/loan.js
+++ routes/loan.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = express.Router()
+const { DateTime } = require("luxon")
 const mySql = require("../lib/mysql-wrapper")
 
 /**
@@ -22,9 +23,52 @@ router.get("/", async (req, res) => {
 
 /**
  * Borrowers requests loans from lender
+ * paymentDuration - contains months where the borrower wants to payback the loaned amount.
  */
 router.post("/request", async (req, res) => {
-  res.json({ initial: "implementation " })
+  let { accessToken, peerListingId, paymentDuration } = req.body
+  if (!accessToken)
+    return res.json({ status: 401, message: "Unauthorized access." })
+
+  const user = await getUser(accessToken.userID)
+  if (user.length === 0)
+    return res.json({ status: 401, message: "Invalid user." })
+
+  if (user[0].is_lender)
+    return res.json({ status: 400, message: "Cannot request a loan as you are a lender." })
+
+  if (!peerListingId)
+    return res.json({ status: 400, message: "Invalid peer listing." })
+
+  if (!paymentDuration || paymentDuration < 1)
+    return res.json({ status: 400, message: "Payment duration cannot be lower than 1 month." })
+
+  try {
+    const peerListing = await getPeerListing(peerListingId)
+    if (peerListing.length === 0)
+      return res.json({ status: 400, message: "Invalid peer listing." })
+
+    const offeredLoans = await getOfferedLoans(accessToken.userID)
+    if (offeredLoans.length > 0)
+      return res.json({ status: 400, message: "You have already been offered a loan, please repay before borrowing again." })
+
+    const { commited_amount, interest_rate, listed_by } = peerListing[0]
+    const dueDate = DateTime.now().plus({ months: paymentDuration })
+    await createLoan({
+      peerListingId,
+      paymentDuration,
+      dueDate: dueDate.toISO(),
+      interest: interest_rate,
+      loanAmount: commited_amount,
+      borrowBy: accessToken.userID,
+      loanBy: listed_by
+    })
+
+    res.json({ status: 200, message: "Loan successfully requested." })
+  } catch (e) {
+    console.error(e)
+    return res.json({ status: 500, message: "Internal server error" })
+  }
 })
 
 /**
@@ -48,6 +92,14 @@ router.post("/repay", async (req, res) => {
   res.json({ initial: "implementation " })
 })
 
+async function getUser (userId) {
+  return mySql.handleQuery(`
+    SELECT *
+    FROM fc_user
+    WHERE user_id = ?
+  `, [userId])
+}
+
 async function getLoansByUser (userId) {
   return mySql.handleQuery(`
     SELECT loans.id, user_id, loan_amount, title as loan_status, loaned_by, payment_duration, due_date, interest
@@ -57,6 +109,31 @@ async function getLoansByUser (userId) {
     WHERE user_id = ?
     ORDER BY loans.id asc;
   `, [userId])
+}
+
+async function getOfferedLoans (userId) {
+  return mySql.handleQuery(`
+    SELECT *
+    FROM loans
+    WHERE loan_status = 2
+    AND user_id = ?
+  `, [userId])
+}
+
+async function getPeerListing (peerListingId) {
+  return mySql.handleQuery(`
+    SELECT *
+    FROM peer_listing
+    WHERE id = ?
+  `, [peerListingId])
+}
+
+async function createLoan ({ peerListingId, interest, paymentDuration, loanBy, loanAmount, borrowBy, dueDate }) {
+  return mySql.handleQuery(`
+    INSERT INTO loans (user_id, loan_amount, loan_status, loaned_by, payment_duration, due_date, interest, peer_listing_id)
+    VALUES
+    (?, ?, ?, ?, ?, ?, ?, ?)
+  `, [borrowBy, loanAmount, 1, loanBy, paymentDuration, dueDate, interest, peerListingId])
 }
 
 module.exports = router
