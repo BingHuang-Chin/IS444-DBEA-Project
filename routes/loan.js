e9bc749aea22030c6d7a17d9cecdc5849108d51e
Bing Huang <bing2609@gmail.com>
2021-11-18 01:38:54 +0800

    routes: loan: repay loan: initial implementation
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- routes/loan.js
+++ routes/loan.js
@@ -171,7 +171,41 @@ router.post("/request/:id/reject", async (req, res) => {
  * Borrowers repay loans one-shot
  */
 router.post("/repay", async (req, res) => {
-  res.json({ initial: "implementation " })
+  let { accessToken } = req.body
+
+  if (!accessToken)
+    return res.json({ status: 401, message: "Unauthorized access." })
+
+  const user = await getUser(accessToken.userID)
+  if (user.length === 0)
+    return res.json({ status: 401, message: "Invalid user." })
+
+  try {
+    const loans = await getOfferedLoans(accessToken.userID)
+    if (loans.length === 0)
+    return res.json({ status: 400, message: "No loans to repay." })
+
+    const { id, loaned_by, loan_amount, interest } = loans[0]
+    const totalPayment = loan_amount + (loan_amount * (interest / 100))
+
+    const { credits: borrowerCredits } = user[0]
+    const { credits: lenderCredits } = (await getUser(loaned_by))[0]
+    if (borrowerCredits < totalPayment)
+      return res.json({ status: 400, message: "Insufficient amount to repay." })
+
+    await transferFastCashCredits({
+      destFinalAmount: lenderCredits + totalPayment,
+      destUserId: loaned_by,
+      sourceFinalAmount: borrowerCredits - totalPayment,
+      sourceUserId: accessToken.userID
+    })
+
+    await repayLoan(id)
+    return res.json({ status: 200, message: "Successfully repaid loan." })
+  } catch (e) {
+    console.error(e)
+    return res.json({ status: 500, message: "Internal server error" })
+  }
 })
 
 async function getUser (userId) {
@@ -268,6 +302,15 @@ async function rejectLoan (loanId) {
     UPDATE loans
     SET
     loan_status = 3
+    WHERE id = ?
+  `, [loanId])
+}
+
+async function repayLoan (loanId) {
+  return mySql.handleQuery(`
+    UPDATE loans
+    SET
+    loan_status = 4
     WHERE id = ?
   `, [loanId])
 }
