ed70cb17b443c9c3a6a628b2a9cb8c9ca99dbe06
Bing Huang <bing2609@gmail.com>
2021-11-17 16:01:46 +0800

    routes: auth: added route to retrieve user information
    Signed-off-by: Bing Huang <bing2609@gmail.com>

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,24 @@ const express = require("express")
 const router = express.Router()
 const mySql = require("../lib/mysql-wrapper")
 
+router.get("/user", async (req, res) => {
+  let { accessToken } = req.query
+  if (!accessToken)
+    return res.json({ status: 401, message: "Unauthorised access." })
+
+  try {
+    accessToken = JSON.parse(accessToken)
+    const users = await getUser(accessToken.userID)
+    if (users.length === 0)
+      return res.json({ status: 401, message: "Unauthorised access." })
+
+    return res.json({ status: 200, data: users[0] })
+  } catch (e) {
+    console.error(e)
+    return res.json({ status: 500, message: "Internal server error" })
+  }
+})
+
 router.post("/login", async (req, res) => {
   let { accessToken, isLender } = req.body
   if (!accessToken)
@@ -20,14 +38,22 @@ router.post("/login", async (req, res) => {
     isLender = is_lender
   }
 
-  res.json({ 
+  res.json({
     status: 200,
     message: "Successfully registered with FastCash.",
     redirect: `/pages/${isLender ? "lender.html" : "borrower.html"}`
   })
 })
 
-async function createUser(userId, isLender) {
+async function getUser (userId) {
+  return mySql.handleQuery(`
+    SELECT *
+    FROM fc_user
+    WHERE user_id = ?
+  `, [userId])
+}
+
+async function createUser (userId, isLender) {
   await mySql.handleQuery(`
     INSERT INTO fc_user (user_id, is_lender)
     VALUES
